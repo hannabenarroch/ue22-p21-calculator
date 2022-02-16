164994543f69b759c2b9d11b743b6c99c8d33824
Hanna Benarroch <hanna.benarroch@mines-paristech.fr>
2022-02-16 20:17:21 +0100

    1st commit

--- subject/script.js
+++ subject/script.js
@@ -3,3 +3,22 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+function printTargetContentToConsole(event) {
+    console.log(event.target.textContent+"!");
+}
+
+window.addEventListener('load', ()=> {
+    let buttons = document.getElementsByTagName("button");
+    let screen = document.getElementsByClassName("calculator__display")[0]
+    for (let button of buttons) {
+        button.addEventListener("click", (event)=> { 
+            console.log(event.target.textContent); 
+let operators = document.getElementsByClassName("key--operator")
+
+            if (screen.textContent=='0') {
+            screen.textContent = event.target.textContent}
+            else {screen.textContent += event.target.textContent}
+        })
+    }
+})
